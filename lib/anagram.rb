16440adcdd66ca7a7228a0a8ebd8e4e4bd276765
aisha-jufe <aishajucfe287>
2023-02-14 14:47:02 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram 
+    
+        def initialize (word)
+            @word = word.downcase
+
+            
+        end
+        def match(word_list)
+            word_list.select { |w| w.downcase.chars.sort == @word.chars.sort && w.downcase != @word }
+          end
+        end
+
